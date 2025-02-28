2b52f5fd76d783a1c05595c061ced7735aaa9c55
etcoder-642 <manassehsamuel642@gmail.com>
2025-03-01 01:42:59 +0300

    minor corrections now it works across devices

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
- const seven = document.querySelector(".seven")
+/**
+ * Things to fix for tomorrow:
+ * 1. There is abug around the percentile button creates
+ *    - when i click 122 and click 122 and click = it will be displayed 244 as it should
+ *    - but if i click % this time it shows me 3.66 instead of 2.44 that means there is some sort of problem around there
+ *    - and i don't even know why i set the checkerForPer flag I have forgot why I putted there I will figure it out later.
+ * 
+ */
+
+const seven = document.querySelector(".seven")
  const btn = Array.from(document.querySelectorAll(".btn-content"))
  const display = document.querySelector(".display")
 
@@ -24,9 +33,9 @@ function operator(fir, opr, sec){
 }
 
 function displayVar(disp){
-    if((disp > 9999999) || (disp < 0.00000001)){
+    if((disp > 9999999) || (disp < 0.000001)){
         display.textContent = disp.toExponential(4);
-    }else if(!(disp > 9999999) && !(disp < 0.00000001) && display.textContent.length >= 10){
+    }else if(!(disp > 9999999) && !(disp < 0.00000001) && disp.toString().length >= 10){
         display.textContent = disp.toString().slice(0, 10)
     }else {
         display.textContent = disp;
@@ -34,7 +43,7 @@ function displayVar(disp){
 }
 
 
- document.addEventListener('click', (e)=>{
+ document.addEventListener('pointerdown', (e)=>{
     let target = e.target; 
     if(target.className.includes("num") && isOprClicked === false){
         display.textContent += display.textContent.length <= 10 ? target.textContent: "";
@@ -82,27 +91,25 @@ function displayVar(disp){
         // console.log(`This is the Operator: ${oprVar}`);
         secVar = ""
     }else if(target.className.includes("per") && (secVar.length === 0)){
-        liveVar = liveVar/100;
-        display.textContent = liveVar;
+        liveVar /= 100;
+        displayVar(liveVar);
     }else if(target.className.includes("per") && !(secVar.length === 0)){
-        if(checkerForPer === false) {
+        // if(checkerForPer === false) {
             let liveVarInt = parseFloat(liveVar);
             let secVarInt = parseFloat(secVar);
             liveVar = operator(liveVarInt, oprVar, secVarInt); 
             checkerForPer = true;    
 
             let temp = liveVar       
-            liveVar = liveVar/100;    
+            liveVar /= 100;    
             // console.log(`This are Bunch of Values: ${liveVarInt} ${oprVar} ${secVarInt} % = ${liveVar}`);
-    
-            display.textContent = liveVar;    
-        }else {
-            let temp = liveVar       
-            liveVar = liveVar/100;
-    
-            // console.log(`This are Bunch of Values: ${temp} % = ${liveVar}`);    
-            display.textContent = liveVar;
-        }
+            displayVar(liveVar);
+        // }else {
+        //     let temp = liveVar       
+        //     liveVar /= 100;
+        //     // console.log(`This are Bunch of Values: ${temp} % = ${liveVar}`);    
+        //     displayVar(liveVar);
+        // }
     }
     if(target.className.includes("alt")) location.reload();
  })
