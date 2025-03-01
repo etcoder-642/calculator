cc3cd095edb1c4ba0644491bee7ff5a501c24e4a
etcoder-642 <manassehsamuel642@gmail.com>
2025-03-01 13:01:11 +0300

    Made minor adjustments => restructured the whole code to make it more readable => chunked the large eventlistener into different functions for readability => made minor adjustments in the CSS

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-/**
- * Things to fix for tomorrow:
- * 1. There is abug around the percentile button creates
- *    - when i click 122 and click 122 and click = it will be displayed 244 as it should
- *    - but if i click % this time it shows me 3.66 instead of 2.44 that means there is some sort of problem around there
- *    - and i don't even know why i set the checkerForPer flag I have forgot why I putted there I will figure it out later.
- * 
- */
+ const MAX_DISPLAY_LENGTH = 10;
+ const MAX_VALUE = 9999999999;
+ const MIN_VAlUE = 0.000001;
 
-const seven = document.querySelector(".seven")
- const btn = Array.from(document.querySelectorAll(".btn-content"))
- const display = document.querySelector(".display")
+ const display = document.querySelector(".display");
 
 
  let liveVar = " ";
@@ -17,7 +10,7 @@ const seven = document.querySelector(".seven")
  let secVar = "";
  let oprVar = " ";
  let checkerForLive = false;
- let checkerForPer = false;
+ let checkerForEqualsInPer = false;
  let checkerForFlag = false;
 
  // operator function
@@ -26,97 +19,143 @@ function operator(fir, opr, sec){
         "\u002B": (a, b) => a + b,
         "\u2212": (a, b) => a - b,
         "\u00F7": (a, b) => a / b,
-        "\u00D7": (a, b) => a * b
+        "\u00D7": (a, b) => a * b,
+        "Xn": (a, b) => Math.pow(a, b)
     };
 
-    return operators[opr] ? operators[opr](fir, sec) : null;
+    if(opr === "\u00F7" && sec === 0){
+        return "U DUMB🤣"
+    }else if (operators[opr](fir, sec) === Infinity){
+        return "BOOM💥"
+    }else if (operators[opr](fir, sec) === NaN){
+        return "STOP🤚"
+    }else {
+        return operators[opr] ? operators[opr](fir, sec) : null;
+    }
 }
 
+// This function handles the display of a value after it have been caluculated by the operator function
+
 function displayVar(disp){
-    if((disp > 9999999) || (disp < 0.000001)){
+    if((disp > MAX_VALUE) || (disp < MIN_VAlUE && disp > 0)){
         display.textContent = disp.toExponential(4);
-    }else if(!(disp > 9999999) && !(disp < 0.00000001) && disp.toString().length >= 10){
-        display.textContent = disp.toString().slice(0, 10)
+    }else if(disp.toString().length > MAX_DISPLAY_LENGTH){
+        display.textContent = disp.toString().slice(0, MAX_DISPLAY_LENGTH);
     }else {
         display.textContent = disp;
     }
 }
 
+// This function handles the display of a value while the user is inputting it
 
- document.addEventListener('click', (e)=>{
-    let target = e.target; 
-    if(target.className.includes("num") && isOprClicked === false){
-        display.textContent += display.textContent.length <= 10 ? target.textContent: "";
-        console.log(display.textContent.length)
-    liveVar += target.textContent; 
+function updateDisplay(text){
+    display.textContent += display.textContent.length <= MAX_DISPLAY_LENGTH ? text: "";
+}
+
+// This function handles a scenario where the number are values are clicked
+
+function handleNumberClick(Input){
+    // The isOprClicked flag checks if an operator is clicked if it is not 
+    // ... it will transport the data to liveVar
+    //... otherwise it will transport user inputted data to secVar
+    if(isOprClicked === false){
+        updateDisplay(Input.textContent);
+        // console.log(display.textContent.length)
+    liveVar += Input.textContent; 
         // console.log(`This is the First variable: ${liveVar}`)
-    }else if(target.className.includes("num") && isOprClicked === true){
+    }else {
         if(checkerForLive === true){
             display.textContent = "";
-            display.textContent += display.textContent.length <= 10 ? target.textContent: "";
+            updateDisplay(Input.textContent);
             checkerForLive = false;
         }else {
-            display.textContent += display.textContent.length <= 10 ? target.textContent: "";
-            console.log(display.textContent.length)
+            updateDisplay(Input.textContent);
+            // console.log(display.textContent.length)
         }
-        secVar += target.textContent; 
+        secVar += Input.textContent; 
         // console.log(`This is the Second variable: ${secVar}`)
-    }else if((target.className.includes("opr") && secVar.length === 0) || (target.className.includes("opr") && checkerForFlag === true)){
+    }
+}
+
+// This function handles a scenario where the operators are clicked
+
+function handleOperatorClick(Input){
+    if((secVar.length === 0) || (checkerForFlag === true)){
         secVar = " ";
         checkerForFlag = false;
         checkerForLive = true;
-        oprVar = target.textContent;
-        display.textContent += oprVar
+        oprVar = Input.textContent;
         // console.log(`This is the Operator: ${oprVar}`);
         isOprClicked = true;
-    }else if(target.className.includes("equals")){
-        let liveVarInt = parseFloat(liveVar);
-        let secVarInt = parseFloat(secVar);
-        liveVar = operator(liveVarInt, oprVar, secVarInt);
-        console.log(`This are Bunch of Values: ${liveVarInt} ${oprVar} ${secVarInt} = ${liveVar}`);
-        display.textContent = liveVar.toString().length <= 10 ? liveVar : liveVar.toString().slice(0, 10);
-        console.log(`${liveVar} ${typeof liveVar}`)
-        displayVar(liveVar);
-        checkerForFlag = true;
-    }else if(target.className.includes("opr") && !(secVar.length === 0)){
+    }else if(!(secVar.length === 0)){
         checkerForLive = true;
 
         let liveVarInt = parseFloat(liveVar);
         let secVarInt = parseFloat(secVar);
         liveVar = operator(liveVarInt, oprVar, secVarInt);
-        console.log(`This are Bunch of Values: ${liveVarInt} ${oprVar} ${secVarInt} = ${liveVar}`);
+        // console.log(`This are Bunch of Values: ${liveVarInt} ${oprVar} ${secVarInt} = ${liveVar}`);
 
         displayVar(liveVar);
-        oprVar = target.textContent;
-        display.textContent += oprVar;
+        oprVar = Input.textContent;
 
         // console.log(`This is the Operator: ${oprVar}`);
         secVar = ""
-    }else if(target.className.includes("per") && (secVar.length === 0)){
+    }
+}
+
+// This function handles a scenario where the equals button is clicked
+
+function handleEqualsClick(){
+    let liveVarInt = parseFloat(liveVar);
+    let secVarInt = parseFloat(secVar);
+    liveVar = operator(liveVarInt, oprVar, secVarInt);
+    // console.log(`This are Bunch of Values: ${liveVarInt} ${oprVar} ${secVarInt} = ${liveVar}`);
+    display.textContent = liveVar.toString().length <= 10 ? liveVar : liveVar.toString().slice(0, 10);
+    // console.log(`${liveVar} ${typeof liveVar}`)
+    displayVar(liveVar);
+    checkerForFlag = true;
+    checkerForEqualsInPer = true;
+}
+
+// handles percentile Click
+
+function handlePercentClick(){
+    if((secVar.length === 0)){
         liveVar /= 100;
         displayVar(liveVar);
-    }else if(target.className.includes("per") && !(secVar.length === 0)){
-        // if(checkerForPer === false) {
+    }else{
+        // the reason checkerForEqualsInPer is used is to prevent edge cases like where the user clicks a number then another number then 
+        // ... the percentage button. Let's say the user clicks 5 then + then 5 then the percentage button the result should computed to 0.1.
+        // ... but since the user have not clicked another operator or the equals button the result of 5 + 5 have not been yet been computed to 10 by the operator function.
+        // ... hence the result was shown to be 0.05. So the checkerForEqualsInPer is used to prevent this edge case by checking if the user have clicked the if
+        // ... equals button was clicked in between. If so it will compute the calculation by itself
+        if(checkerForEqualsInPer === false) {
             let liveVarInt = parseFloat(liveVar);
             let secVarInt = parseFloat(secVar);
             liveVar = operator(liveVarInt, oprVar, secVarInt); 
-            checkerForPer = true;    
+            checkerForEqualsInPer = true;    
 
-            let temp = liveVar       
             liveVar /= 100;    
             // console.log(`This are Bunch of Values: ${liveVarInt} ${oprVar} ${secVarInt} % = ${liveVar}`);
             displayVar(liveVar);
-        // }else {
-        //     let temp = liveVar       
-        //     liveVar /= 100;
-        //     // console.log(`This are Bunch of Values: ${temp} % = ${liveVar}`);    
-        //     displayVar(liveVar);
-        // }
+        }else {
+            liveVar /= 100;
+            // console.log(`This are Bunch of Values: ${temp} % = ${liveVar}`);    
+            displayVar(liveVar);
+        }
     }
-    if(target.className.includes("alt")) location.reload();
+}
+
+ document.addEventListener('click', (e)=>{
+    let target = e.target; 
+    if(target.className.includes("num")){
+        handleNumberClick(target);
+    }else if(target.className.includes("opr")){
+        handleOperatorClick(target);
+    }else if(target.className.includes("equals")){
+        handleEqualsClick();
+    }else if(target.className.includes("per")){
+        handlePercentClick();
+    }else if(target.className.includes("alt")) location.reload();
  })
 
- 
-function addText(btn){
-     btn.textContent;
- }
